82e7033cff721b0e9bb47bdc05c75d6fa93bf163
nisuev <nisuev04@mail.ru>
2024-04-11 02:27:56 +0300

    changes in hexviewer

--- hexviewer/src/hexer.rs
+++ hexviewer/src/hexer.rs
@@ -1,12 +1,34 @@
+use ansi_term::Colour;
 use std::{fs::File, io::Read};
 
 const BYTES_PER_LINE: usize = 16;
 
-fn print_address(address: usize) { print!{"{:08X} ", address}; }
+const LINK_SEP: &str = "+";
+const HORIZONT_SEP: &str = "-";
+const VERTICAL_SEP: &str = "|";
+
+const BYTE_CLR: Colour = Colour::RGB(0xFF, 0xFF, 0xFF);
+const DECODE_CLR: Colour = Colour::RGB(0xFF, 0xFF, 0xFF);
+const ADRESS_CLR: Colour = Colour::RGB(0xFF, 0xFF, 0xFF);
+
+fn horizontal_bord() {
+    println!("{LINK_SEP}{0}{LINK_SEP}{1}{LINK_SEP}{2}{LINK_SEP}", 
+              HORIZONT_SEP.repeat(8), 
+              HORIZONT_SEP.repeat(BYTES_PER_LINE * 3 + 2 * 1), 
+              HORIZONT_SEP.repeat(BYTES_PER_LINE)
+            )
+}
+
+fn print_address(address: usize) { print!{"{VERTICAL_SEP}{}{VERTICAL_SEP} ", ADRESS_CLR.italic().paint(format!("{:08X}", address))}; }
 
 fn print_bytes_hex(bytes: &[u8; BYTES_PER_LINE], bytes_cnt: usize) {
+    let mut printed = 0; 
     for byte in bytes.iter().take(bytes_cnt) {
-        print!("{:02X} ", byte);
+        print!("{} ", BYTE_CLR.paint(format!("{:02X}", byte)));
+        printed += 1;
+        if printed == BYTES_PER_LINE / 2 {
+            print!(" ");
+        }
     }
 
     for _ in 0..(BYTES_PER_LINE - bytes_cnt) {
@@ -18,18 +40,22 @@ fn print_bytes_dec(bytes: &[u8; BYTES_PER_LINE], bytes_cnt: usize) {
     for byte in bytes.iter().take(bytes_cnt) {
         let symb = *byte as char;
         if symb.is_ascii_graphic() {
-            print!("{}", symb);
+            print!("{}", DECODE_CLR.italic().paint(symb.to_string()));
         } else {
             print!(".");
         }
+    }
+
+    for _ in 0..(BYTES_PER_LINE - bytes_cnt) {
+        print!(" ");
     }
 }
 
 fn print_bytes(bytes: &[u8; BYTES_PER_LINE], bytes_cnt: usize) {
     print_bytes_hex(bytes, bytes_cnt);
-    print!("   ");
+    print!("{VERTICAL_SEP}");
     print_bytes_dec(bytes, bytes_cnt);
-    println!();
+    println!("{VERTICAL_SEP}");
 }
 
 pub fn hex_viewer(file: &mut File) -> Result<(), ()> {
@@ -37,6 +63,7 @@ pub fn hex_viewer(file: &mut File) -> Result<(), ()> {
 
     // Читаем данные из файла в буфер и выводим их в шестнадцатеричном виде
     let mut address = 0;
+    horizontal_bord();
     loop {
         let bytes_read_cnt = match file.read(&mut bytes) {
             Ok(cnt) => cnt,
@@ -48,5 +75,6 @@ pub fn hex_viewer(file: &mut File) -> Result<(), ()> {
         address += bytes_read_cnt;
         print_bytes(&bytes, bytes_read_cnt);
     }
+    horizontal_bord();
     Ok(())
 }
